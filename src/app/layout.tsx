df1f4357eb80a01dd2d5b37b0c507dbf97d073ca
Gabriele Polli <gpolli.dev@gmail.com>
2026-03-16 22:48:11 +0100

    chore(graphql): :hammer: setup React Query + GraphQL

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,24 @@
-import './globals.css';
+import './globals.css'
 
-import type { Metadata } from 'next';
+import type { Metadata } from 'next'
+
+import Providers from './providers'
 
 export const metadata: Metadata = {
   title: 'My App',
   description: 'Next.js + TypeScript + Storybook + Vitest',
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  );
+  )
 }
