bcff55a00cce291546b304754a74cb4dea00055e
Gabriele Polli <gpolli.dev@gmail.com>
2026-03-29 16:33:43 +0200

    feat(app): :sparkles: create context or props experiment page

---
+++ src/app/context-or-props/page.tsx
@@ -0,0 +1,5 @@
+import { ContextOrPropsPage } from '@/experiments/context-or-props'
+
+export default function RenderingPage() {
+  return <ContextOrPropsPage />
+}
--- src/app/rerender-propagation/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Experiment1Page } from '@/experiments/ctx-vs-prop-drilling'
-
-export default function RenderingPage() {
-  return <Experiment1Page />
-}
